43cc57c5b8e846cea318556f02da75550114034c
Kostyantin Savchuk <kostya@gyry.com.ua>
2019-04-29 02:03:45 +0300

    Update build_boot_logo.sh

--- build_boot_logo.sh
+++ build_boot_logo.sh
@@ -4,4 +4,6 @@
 source build/envsetup.sh
 lunch kvim2-userdebug-64
 make logoimg
+
+# Package Update Image
 ./vendor/amlogic/tools/aml_upgrade/aml_image_v2_packer  -r out/target/product/kvim2/upgrade/aml_upgrade_package.conf  out/target/product/kvim2/upgrade/ out/target/product/kvim2/update.img

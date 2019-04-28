41971e949c53f5d2119a67a817741ca35f3d5ac9
Kostyantin Savchuk <kostya@gyry.com.ua>
2019-04-29 02:05:08 +0300

    Create build_system.sh

---
+++ build_system.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Rebuild System Image
+source build/envsetup.sh
+lunch kvim2-userdebug-64
+make snod
+
+# Package Update Image
+./vendor/amlogic/tools/aml_upgrade/aml_image_v2_packer  -r out/target/product/kvim2/upgrade/aml_upgrade_package.conf  out/target/product/kvim2/upgrade/ out/target/product/kvim2/update.img

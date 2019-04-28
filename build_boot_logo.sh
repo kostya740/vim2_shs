42d34e5d5918a3efc5a4848969dd905b3b95be97
Kostyantin Savchuk <kostya@gyry.com.ua>
2019-04-29 02:01:55 +0300

    Create build_boot_logo.sh

---
+++ build_boot_logo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Rebuild Logo Image
+source build/envsetup.sh
+lunch kvim2-userdebug-64
+make logoimg
+./vendor/amlogic/tools/aml_upgrade/aml_image_v2_packer  -r out/target/product/kvim2/upgrade/aml_upgrade_package.conf  out/target/product/kvim2/upgrade/ out/target/product/kvim2/update.img

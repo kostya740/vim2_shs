7523c643ebee5c320453997384a13de278da0ec2
Kostyantin Savchuk <kostya@gyry.com.ua>
2019-04-29 01:55:48 +0300

    Create full_build.sh

---
+++ full_build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# build uboot
+cd uboot
+make CROSS_COMPILE=aarch64-linux-gnu- kvim2_defconfig
+make CROSS_COMPILE=aarch64-linux-gnu-
+
+# build android
+cd ../
+source build/envsetup.sh
+lunch kvim2-userdebug-64
+make -j2 otapackage
